0467debc365036d57b8b2bf89ee3a337d3cf19ac
Dave Hall <skwashd@gmail.com>
2018-07-22 16:32:24 +1000

    Add apk-tools and cleanup variables
    * Add apk-tools so packages can be installed
    * Use a variable for packages to install
    * Format variables more consistently

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 set -e
 
 ALPINE_VER="3.8"
+PACKAGES="apk-tools ca-certificates ssl_client"
+
+
 MKROOTFS="/tmp/alpine-make-rootfs"
 BUILD_TAR="/tmp/docker/alpine-rootfs-${ALPINE_VER}.tar.gz"
 DOCKER_ROOT=$(dirname $BUILD_TAR)
@@ -11,21 +14,21 @@ POST_INSTALL="./post-install.sh"
 mkdir $DOCKER_ROOT
 
 # Download rootfs builder and verify it.
-wget https://github.com/alpinelinux/alpine-make-rootfs/raw/af6880d17404e9811592092f0f8eb60959869ef5/alpine-make-rootfs -O ${MKROOTFS}
-echo "c93db7105060fb4227eeaaf9a98555308913090f71ece86d28eee8a376ab439f  ${MKROOTFS}" | sha256sum -c -
+wget https://github.com/alpinelinux/alpine-make-rootfs/raw/af6880d17404e9811592092f0f8eb60959869ef5/alpine-make-rootfs -O "$MKROOTFS"
+echo "c93db7105060fb4227eeaaf9a98555308913090f71ece86d28eee8a376ab439f  $MKROOTFS" | sha256sum -c -
 chmod +x ${MKROOTFS}
 
 sudo ${MKROOTFS} --mirror-uri http://dl-cdn.alpinelinux.org/alpine/ \
 	--branch "v${ALPINE_VER}" \
-	--packages 'ssl_client ca-certificates' \
+	--packages "$PACKAGES" \
 	--script-chroot \
-	${BUILD_TAR} \
-	${POST_INSTALL}
+	"$BUILD_TAR" \
+	"$POST_INSTALL"
 
 cat <<DOCKERFILE > /tmp/docker/Dockerfile
 FROM scratch
 USER worker
-ADD $(basename ${BUILD_TAR}) /
+ADD $(basename $BUILD_TAR) /
 CMD ["/bin/sh"]
 DOCKERFILE
 
